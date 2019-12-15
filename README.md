91264689ed75fcf2c51345fb9a986137a3711436
lukaszc27 <lukaszciesla52@gmail.com>
2019-12-15 13:40:16 +0100

    Add SQL syntax to create database

--- README.md
+++ README.md
@@ -1,9 +1,15 @@
 # LibraryWPF is C# WPF Project
 
+## Warnings
+Before compilation, you must change the name of 
+the MSSQL server to which the application connects in the file:
+`DatabaseClient/Database.cs (line 46)`
+
 ## Technologies
 Project is createc with:
 * C#
 * XAML
+* SQL (MSSQL)
 
 ## Created
 * [DatabaseClient](#DatabaseClient)
---
+++ database.sql
@@ -0,0 +1,30 @@
+CREATE DATABASE LibraryWPF;
+GO
+USE LibraryWPF;
+GO
+
+CREATE TABLE Books (
+    id      BIGINT IDENTITY(1, 1) PRIMARY KEY,
+    title   NVARCHAR(128) NOT NULL,
+    author  NVARCHAR(64),
+    isbn    VARCHAR(128) UNIQUE
+);
+
+CREATE TABLE Students (
+    id          BIGINT IDENTITY(1, 1) PRIMARY KEY,
+    name        NVARCHAR(32) NOT NULL,
+    surname     NVARCHAR(32) NOT NULL,
+    albumNumber BIGINT NOT NULL UNIQUE
+);
+GO;
+
+CREATE TABLE Rents (
+    id          BIGINT IDENTITY(1, 1),
+    startDate   DATE NOT NULL,
+    endDate     DATE,
+    bookId      BIGINT NOT NULL,
+    studentId   BIGINT NOT NULL,
+
+    FOREIGN KEY (bookId) REFERENCES Books(id),
+    FOREIGN KEY (studentId) REFERENCES Students(id)
+);
