188ccfb4982e8a854e647bcbc9176bc9c1a32c3a
Jakub Firlej <jakub.firlej@outlook.com>
2020-01-11 21:25:25 +0100

    create project with unity tests

---
+++ DatabaseClientTest/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DatabaseClientTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
