76130cc55e20015379c9f327a8fc18f3f0ddec17
Jakub Firlej <jakub.firlej@outlook.com>
2020-01-11 22:51:08 +0100

    Add test students ,books

---
+++ DatabaseClientTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DatabaseClientTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DatabaseClientTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("19ec2077-d74f-4872-8307-501eaef6273c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DatabaseClientTests/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DatabaseClient;
+using System.Data.SqlClient;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DatabaseConnectionTest()
+        {
+            Database database = new Database();
+
+            {
+                
+
+                // testy na studencie
+                Student.Create("Test", "Student", 999);
+
+                Assert.IsTrue(Student.All().Count > 0);
+                Assert.IsNotNull(Student.FindByAlbumNumber(999));
+
+                // testy na książkach
+                Book.Create("1234-567-890-1", "Test Book", "UnitTest");
+
+                Assert.IsTrue(Book.All().Count > 0);
+                Assert.IsNotNull(Book.Find("1234-567-890-1"));
+            }
+            
+        }
+        [TestMethod]
+        public void StudentsTwoObjectsSame()
+        {
+            Student student1 = new Student("Jan", "Nowak");
+            Student student2 = new Student("Jan", "Nowak");
+
+            Assert.AreEqual(student1, student2);
+        }
+
+        [TestMethod]
+        public void StudentsTwoObjectsDifferent()
+        {
+            Student student1 = new Student("Jan", "Nowak");
+            Student student2 = new Student("Janusz", "Nosacz");
+
+            Assert.AreNotEqual(student1, student2);
+        }
+
+        [TestMethod]
+        public void StudentsOperatorAssign()
+        {
+            Student student1 = new Student("Janusz", "Nosacz");
+            Student student2 = student1;
+
+            Assert.AreEqual(student1, student2);
+        }
+    }
+}
